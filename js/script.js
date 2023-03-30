d034853231bde7741b73ac9a2565bc6fc348d79a
olexandr_rd <sasha2002rudenko@gmail.com>
2023-03-30 23:51:31 +0300

    shop page, sort blocks function

--- js/script.js
+++ js/script.js
@@ -5,9 +5,21 @@ function showMenu() {
     menu.classList.toggle('show-menu');
     navSpans.forEach(element => element.classList.toggle('open'));
 }
-    
+
 function subscribe(event) {
     let subscribeForm = document.getElementById('subscribe');
     let validationMessage = !subscribeForm.checkValidity() ? 'Некоректна email адреса!' : 'Успішно підписано!';
     document.getElementById('subscribe-message').innerHTML = validationMessage;
+}
+
+function showSorting(blockId) {
+    let block = document.getElementById(blockId);
+    if (block.style.display === "block") {
+        block.style.display = "none"
+    }
+    else {
+        let blocks = Array.from(document.getElementById("sorting-blocks").children);
+        blocks.forEach(element => element.style.display = "none");
+        document.getElementById(blockId).style.display = "block";
+    }
 }
